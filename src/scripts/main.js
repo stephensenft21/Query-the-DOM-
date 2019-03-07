85ded103382c9c5711cc66a730513a58194213f1
Stephen Senft <stephensenft21@gmail.com>
2019-03-07 12:02:12 -0600

    adding final changes made to JS file

---
+++ src/scripts/main.js
@@ -0,0 +1,22 @@
+
+
+
+// const articleHeader = document.querySelector(".article__header");
+// articleHeader.textContent = "Welcome to Stephen's Blog";
+
+// const header = document.querySelectorAll(".article__header");
+
+// for (let i = 0; i < header.length; i++){
+//   header[i].classList.add("important");
+// }
+
+// const dashed = document.querySelector(".dashed");
+// dashed.remove();
+
+// const footer = document.querySelector(".article__footer");
+// footer.classList.add("goldenrod");
+
+
+
+
+const articleHeader = document 
